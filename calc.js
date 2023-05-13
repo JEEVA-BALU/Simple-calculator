35c18e31f96e94702a4d4f13c9f2bcdf4fcd014d
JEEVA-BALU <112182185+JEEVA-BALU@users.noreply.github.com>
2023-05-13 20:19:58 +0530

    calc.js

--- calc.js
+++ calc.js
@@ -12,7 +12,7 @@ buttons.map( button => {
                 try{
                     display.innerText = eval(display.innerText);
                 } catch {
-                    display.innerText = "Error"
+                    display.innerText = "Error";
                 }
                 break;
             case '←':
